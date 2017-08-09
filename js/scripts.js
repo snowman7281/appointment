e0a6f94c0f28ceeecd639386fa0fbb4812626874
luciano oliveira and snow vilay <luciano@>
2017-08-09 15:03:01 -0700

    add a UI like a appointment setter using form, date, time functions.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+
+  // $("#submit").click(function(){
+    $("#name").submit(function(e){
+      var person = $("#person").val();
+      var description = $("#description").val();
+      var date = $("#date").val();
+      var newDate = new Date(date);
+      var time = $("#time").val();
+      var $output = $(".output");
+      $("#name").hide();
+      $output.text("Thank you " + person + " for setting an appointment on " + newDate + " at " + time);
+
+      e.preventDefault();
+    });
+  // });
+});
